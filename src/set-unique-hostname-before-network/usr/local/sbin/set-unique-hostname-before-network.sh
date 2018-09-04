e4a4e2eedbd1c793503d8da7f49605de175d8ef8
Jason Stanford <jps3@lehigh.edu>
2018-09-04 08:43:10 -0400

    Cleaned up set-unique-hostname-before-network.service

--- src/set-unique-hostname-before-network/usr/local/sbin/set-unique-hostname-before-network.sh
+++ src/set-unique-hostname-before-network/usr/local/sbin/set-unique-hostname-before-network.sh
@@ -2,16 +2,27 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
-hostname_prefix=""
-net_iface="eth0"
-net_iface_mac_nic="$(cat /sys/class/net/${net_iface}/address | cut -d: -f4- | tr -d : | tr 'a-z' 'A-Z')"
+set -e
+set -E
 
-new_hostname="${hostname_prefix}-${net_iface_mac_nic}"
+NETWORK_IFACE_MAC_NIC="$(cat /sys/class/net/${NETWORK_IFACE}/address | cut -d: -f4- | tr -d : | tr 'a-z' 'A-Z')"
+NEW_HOSTNAME="${HOSTNAME_PREFIX}-${NETWORK_IFACE_MAC_NIC}"
 
-echo $new_hostname > /proc/sys/kernel/hostname
-echo $new_hostname > /etc/hostname
+#
+# Simple check that results in existing with an error if any spaces
+# are found in $NEW_HOSTNAME
+#
+echo "$NEW_HOSTNAME" | grep -v -q '[[:space:]]'
+
+#
+# Set the new hostname
+#
+echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
+echo $NEW_HOSTNAME > /etc/hostname
 /bin/hostname -F /etc/hostname
 
-if [[ ! -e /etc/salt/minion_id ]]; then
-	echo $new_hostname > /etc/salt/minion_id
-fi
+set +E
+set +e
+
+exit 0
+
