c7e9b7459ba9e98c24ebd58d6f7bd626f5975751
Jason Stanford <jps3@lehigh.edu>
2018-10-22 21:56:40 -0400

    Many code fixes and much cleanup to prepare-updated-3dprinteros-img.sh
     - Updated gitignore file to include *.xz
     - Added an ssh-pubkeys folder for use by prepare scripts
     - Move prep script ANSI colors and logging to own file
     - Initial import of (ostensibly) saltstack-focused img prep script
     - Updated .gitignore for ssh-pubkeys folder
     - ANSI color defs and logging functions moved to external file
     - Img file partition resizing commented out
     - There had been issues with recent Raspbian builds when using these sections as currently written which prevented successful booting.
     - Removed systemd-nspawn commands to SYSTEMD_NSPAWN_CMD
     - Updated and fixed issues with random root password generation and saving
     - Cleaned up chattr-related array and app code
     - Quoted each additional_packages item
     - This should avoid any odd interpretation of names with '-' at points in the scripts.
     - Moved logging functions to external script
     - Commented out apt-mark for raspberrypi-sys-mods pkg

--- src/prepare-updated-3dprinteros-img.sh
+++ src/prepare-updated-3dprinteros-img.sh
@@ -12,74 +12,44 @@ set -E
 #
 # =====================================================================
 
+SYSTEMD_NSPAWN_CMD='systemd-nspawn -q --bind /usr/bin/qemu-arm-static --bind /vagrant -D ./rootfs/'
+
 timestamp_start="$(date +"%s")"
 
 orig_img="${1:-3DPrinterOS.img}"
 
 temp_img=$(mktemp ${orig_img//.img}-XXXXXX.img)
-temp_img_resize_amt="+250M" # qemu-img units and syntax
+#temp_img_resize_amt="+250M" # qemu-img units and syntax
 temp_img_password_file="${temp_img//.img}-root-password.txt"
 
-random_root_password=""
-random_root_password_hash=""
+random_root_password_length=20
 
 additional_packages=( 
-    apt-transport-https
-    ca-certificates
-    jq
-    lsb-release
-    rsync
+    "apt-transport-https"
+    "ca-certificates"
+    "jq"
+    "lsb-release"
+    "rsync"
   )
 
+#files_to_chattr=(
+#  "etc/passwd"
+#  "etc/passwd-"
+#  "etc/shadow"
+#  "etc/shadow-"
+#  "etc/ssh/sshd_config"
+#  "root/.ssh/authorized_keys"
+#  )
+
+#purge_packages=()
 
 # =====================================================================
 #
-# ANSI Color Codes
+# Source additional "libs"
 #
 # =====================================================================
 
-START='\e[1m'
-END='\e[0m'
-
-#BLACK='\e[0;30m'
-#BOLDBLACK='\e[1;30m'
-#HIBLACK='\e[0;90m'
-#BOLDHIBLACK='\e[1;90m'
-
-RED='\e[0;31m'
-BOLDRED='\e[1;31m'
-HIRED='\e[0;91m'
-BOLDHIRED='\e[1;91m'
-
-GREEN='\e[0;32m'
-BOLDGREEN='\e[1;32m'
-HIGREEN='\e[0;92m'
-BOLDHIGREEN='\e[1;92m'
-
-YELLOW='\e[0;33m'
-BOLDYELLOW='\e[1;33m'
-HIYELLOW='\e[0;93m'
-BOLDHIYELLOW='\e[1;93m'
-
-CYAN='\e[0;34m'
-BOLDCYAN='\e[1;34m'
-HIBLUE='\e[0;94m'
-BOLDHIBLUE='\e[1;94m'
-
-#PURPLE='\e[0;35m'
-#BOLDPURPLE='\e[1;35m'; 
-#HIPURPLE='\e[0;95m'
-#BOLDHIPURPLE='\e[1;95m'
-
-CYAN='\e[0;36m'
-BOLDCYAN='\e[1;36m'
-HICYAN='\e[0;96m'
-BOLDHICYAN='\e[1;96m'
-
-WHITE='\e[0;37m'
-BOLDWHITE='\e[1;37m'
-HIWHITE='\e[0;97m'
-BOLDHIWHITE='\e[1;97m'
+source $(dirname $0)/lib-colors-logging.sh
 
 
 # =====================================================================
@@ -88,62 +58,13 @@ BOLDHIWHITE='\e[1;97m'
 #
 # =====================================================================
 
-function info () {
-  local _key="$1"
-  local _value="$2"
-  echo -en '['${START}${BOLDCYAN}'INFO '${END}']: '
-  echo -en ${WHITE}
-  printf "%-30s : " "${_key}"
-  echo -en ${END}
-  echo -e ${BOLDWHITE}${START}"${_value}"${END}
-}
-
-function log () {
-  echo -e '['${START}${BOLDGREEN}'LOG'${END}'  ]: '${WHITE}"$*"${END}
-}
-
-function warn () {
-  echo -e '['${START}${BOLDYELLOW}'WARN'${END}' ]: '${WHITE}"$*"${END}
-}
-
-function error () {
-  echo -e '['${START}${BOLDRED}'ERROR'${END}']: '${START}${BOLDHIRED}"$*"${END}
-}
-
-function debug () {
-  echo -e '['${START}${BOLDHIWHITE}'DEBUG'${END}']: '${START}${WHITE}"$*"${END}
-}
-
-function error_and_exit () {
-  error "$*"
-  exit 1
-}
-
-function print_header () {
-  # 
-  # ├─┤ HEADER TEXT ├──────────────────────────────────────┤
-  # 
-  local _header="$(echo "$*" | tr 'a-z' 'A-Z')"
-  local _width=$(( $(tput cols) - "${#1}" - 7 ))
-  echo -e '\n'
-  echo -en ${START}${WHITE}
-  echo -n $'\u251c\u2500\u2524 '         # ├─┤
-  echo -en ${END}
-  echo -en ${START}${BOLDHICYAN}"${_header}"${END}
-  echo -en ${START}${WHITE}
-  echo -n $' \u251c'                     # ├
-  eval printf $'%.0s\u2500' {1..$_width} #  ──
-  echo -n $'\u2524'                      #    ┤
-  echo -en ${END}
-  echo -e '\n'
-}
-
 function err_handler () {
   error "The last command returned $?."
   # TODO: what needs to be handled insofar as mounted images, etc etc etc
 }
 
 function cleanup_and_exit () {
+  set +x
   print_header "Exiting & Cleanup"
   log "Cleaning up before exit ..."
 
@@ -194,6 +115,8 @@ info "Original img file" "$orig_img"
 
 if [[ $temp_img =~ .*-updated.*.img ]]; then
   warn "Filename contains '-update', assuming customized image ..."
+  debug "Removing initial temporary file ${temp_img} ..."
+  rm -f $temp_img
   temp_img="$(echo $temp_img | sed -e 's/-updated/-updated-custom/')"
 fi
 
@@ -202,73 +125,82 @@ log  "Copying $orig_img to $temp_img ...\n"
 pv $orig_img > $temp_img
 
 
-# =====================================================================
-#
-# Enlarge (resize) image file if not an -update.img file
-#
-# =====================================================================
+# # =====================================================================
+# #
+# # Enlarge (resize) image file if not an -update.img file
+# #
+# # =====================================================================
+# 
+# print_header "Resize Image File"
+# 
+# if [[ $temp_img =~ .*-updated.*.img ]]; then
+#   warn "Skipping. Filename contains '-update' ..."
+# else
+#   log "Resizing $temp_img by $temp_img_resize_amt ..."
+#   info "Original size" "$(stat -c '%s' $temp_img)"
+#   qemu-img resize -f raw $temp_img $temp_img_resize_amt >/dev/null
+#   info "New size" "$(stat -c '%s' $temp_img)"
+# fi
 
-print_header "Resize Image File"
 
-if [[ $temp_img =~ .*-updated.*.img ]]; then
-  warn "Skipping. Filename contains '-update' ..."
-else
-  log "Resizing $temp_img by $temp_img_resize_amt ..."
-  info "Original size" "$(stat -c '%s' $temp_img)"
-  qemu-img resize -f raw $temp_img $temp_img_resize_amt >/dev/null
-  info "New size" "$(stat -c '%s' $temp_img)"
-fi
-
-
-# =====================================================================
-#
-# Expand size of Linux partition to fill new img size
-#
-# =====================================================================
-
-print_header "Expand Linux Partition to Fill New Image Size"
-
-if [[ $temp_img =~ .*-updated.*.img ]]; then
-
-  warn "Skipping. Filename contains '-update' ..."
-
-else
-
-  start="$(    get_img_partition_info | awk -F: '$1==2       && $5=="ext4" { print $2 }')"
-  orig_end="$( get_img_partition_info | awk -F: '$1==2       && $5=="ext4" { print $3 }')"
-  new_end="$(  get_img_partition_info | awk -F: '$1~/\.img$/ && $3=="file" { print $2 }')"
-  new_end=$(( ${new_end//s} - 1 )) # parted is happier if we back off by one
-
-  info "Starting sector is"           "${start//s}"
-  info "Original ending sector is"    "${orig_end//s}"
-  info "New ending sector of img is"  "${new_end}"
-
-  log "Resizing Linux partition ..."
-  /sbin/parted -s $temp_img \
-    unit s \
-    rm 2 \
-    mkpart primary ext4 $start $new_end \
-    >/dev/null
-
-  log "Creating partition mappings from image file ..."
-  sudo kpartx -av $temp_img
-
-  log "... Sleeping for 1 second ..."
-  sleep 1
-
-  log "Checking filesystem of image's resized Linux partition ..."
-  sudo e2fsck -f /dev/mapper/loop0p2
-
-  log "Resizing filesystem of image's resized Linux partition to fill new, empty space ..."
-  sudo resize2fs /dev/mapper/loop0p2
-
-  log "... Sleeping for 1 second ..."
-  sleep 1
-
-  log "Deleting partition mappings for image file ..."
-  sudo kpartx -dv $temp_img
-
-fi
+# # =====================================================================
+# #
+# # Expand size of Linux partition to fill new img size
+# #
+# # =====================================================================
+# 
+# print_header "Expand Linux Partition to Fill New Image Size"
+# 
+# if [[ $temp_img =~ .*-updated.*.img ]]; then
+# 
+#   warn "Skipping. Filename contains '-update' ..."
+# 
+# else
+# 
+#   start="$(    get_img_partition_info | \
+#     awk -F: '$1==2       && $5=="ext4" { print $2 }')"
+# 
+#   orig_end="$( get_img_partition_info | \
+#     awk -F: '$1==2       && $5=="ext4" { print $3 }')"
+# 
+#   new_end="$(  get_img_partition_info | \
+#     awk -F: '$1~/\.img$/ && $3=="file" { print $2 }')"
+#   new_end=$(( ${new_end//s} - 1 )) # parted is happier if we back off by one
+# 
+#   info "Starting sector is"           "${start//s}"
+#   info "Original ending sector is"    "${orig_end//s}"
+#   info "New ending sector of img is"  "${new_end}"
+# 
+#   log "Resizing Linux partition ..."
+#   /sbin/parted -s $temp_img \
+#     unit s \
+#     rm 2 \
+#     mkpart primary ext4 $start $new_end \
+#     >/dev/null
+# 
+#   log "Creating partition mappings from image file ..."
+#   read -a loop_maps <<< $( 
+#     sudo kpartx -av $temp_img | \
+#     awk '$0~/^add map/ { printf("%s ",$3) }'
+#   )
+#   debug 'loop_maps=( '"${loop_maps[@]}"' )'
+# 
+#   log "... Sleeping for 1 second ..."
+#   sleep 1
+# 
+#   log "Checking filesystem of image's resized Linux partition ..."
+#   sudo e2fsck -f /dev/mapper/"${loop_maps[1]}"
+# 
+#   log "Resizing image's Linux partition to fill extended space ..."
+#   sudo resize2fs /dev/mapper/"${loop_maps[1]}"
+# 
+#   log "... Sleeping for 1 second ..."
+#   sleep 1
+# 
+#   log "Deleting partition mappings for image file ..."
+#   sudo kpartx -dv $temp_img
+# 
+# fi
 
 
 # =====================================================================
@@ -280,7 +212,11 @@ fi
 print_header "Mount Image"
 
 log "Creating partition mappings for image file ..."
-sudo kpartx -av $temp_img
+read -a loop_maps <<< $( 
+  sudo kpartx -av $temp_img | \
+  awk '$0~/^add map/ { printf("%s ",$3) }'
+)
+debug 'loop_maps=( '"${loop_maps[@]}"' )'
 
 log "... Sleeping for 1 second ..."
 sleep 1
@@ -289,10 +225,10 @@ log "Making rootfs mountpoint ..."
 mkdir -p rootfs
 
 log "Mounting to rootfs ..."
-sudo mount -v /dev/mapper/loop0p2 rootfs
+sudo mount -v /dev/mapper/"${loop_maps[1]}" rootfs
 
 log "Mounting to rootfs/boot ..."
-sudo mount -v /dev/mapper/loop0p1 rootfs/boot
+sudo mount -v /dev/mapper/"${loop_maps[0]}" rootfs/boot
 
 
 # =====================================================================
@@ -303,23 +239,36 @@ sudo mount -v /dev/mapper/loop0p1 rootfs/boot
 
 print_header "Update Root Password on Image"
 
-random_root_password="$(pwgen -s -cnyB 25 1 | tr -d \'\"\!)"
-random_root_password_hash="$(python3 -c 'import sys; import crypt; print(crypt.crypt("'"${random_root_password}"'", crypt.mksalt(crypt.METHOD_SHA512)))')"
-info "New root password will be"  "$random_root_password"
-info "Password hash is"           "$random_root_password_hash"
-
+log "Generating new random password for root user on image ..."
 set -o noclobber
-echo "$random_root_password"      > $temp_img_password_file
-echo "$random_root_password_hash" >> $temp_img_password_file
+/usr/bin/env python3 <<EOF > $temp_img_password_file
+import base64
+import crypt
+import random
+import string
+password_len = ${random_root_password_length:-20}
+all_chars = string.ascii_letters + string.punctuation + string.digits
+password = "".join(random.choice(all_chars) for x in range(0, password_len))
+encoded = base64.b64encode(password.encode())
+hash = crypt.crypt(password, crypt.mksalt(crypt.METHOD_SHA512))
+print("{p}\n{e}\n{h}\n".format(p=password, e=encoded, h=hash))
+EOF
 set +o noclobber
 
-info "Root password saved to"     "$temp_img_password_file"
+log "A copy of the cleartext password and hash saved to $temp_img_password_file"
 
-log "Changing root password on image ..."
-sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash <<-EOF
-  echo 'root:${random_root_password_hash}' | chpasswd -e
-  sleep 1
+random_root_password_hash="$(sed -n '/^\$6\$/p' $temp_img_password_file)"
+
+if [[ -n "${random_root_password_hash}" ]]; then
+  log "Changing root password on image ..."
+  sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash <<-EOF
+    echo 'root:${random_root_password_hash}' | chpasswd -e
+    sleep 1
 EOF
+  log "Password changed for root on image."
+else
+  error "\$random_root_password_hash variable is empty."
+fi
 
 
 # =====================================================================
@@ -337,7 +286,7 @@ if [[ $temp_img =~ .*-updated.*.img ]]; then
 else
 
   log "Updating system configurations ..."
-  sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash <<-EOF
+  sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash <<-EOF
     export DEBIAN_FRONTEND=noninteractive
     echo 'America/New_York' > /etc/timezone
     dpkg-reconfigure -f noninteractive tzdata
@@ -349,34 +298,53 @@ else
 EOF
 
   log "Updating packages (this may take awhile) ..."
-  sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash <<-EOF
+  sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash <<-EOF
     echo 'Acquire::http { Proxy "http://172.17.0.1:3142"; };' | tee /etc/apt/apt.conf.d/51cache
     export http_proxy="http://172.17.0.1:3142"
     apt-get update          -qq
-    apt-mark               hold  raspberrypi-sys-mods
-    apt-get upgrade         -qq
-    apt-mark             unhold  raspberrypi-sys-mods
+    # apt-mark               hold  raspberrypi-sys-mods
+    # apt-get upgrade         -qq
+    # apt-mark             unhold  raspberrypi-sys-mods
     apt-get upgrade         -qq
     apt-get dist-upgrade    -qq
-    apt-get purge           -qq  ${purge_packages[@]}
+    # apt-get purge           -qq  ${purge_packages[@]}
     apt-get autoremove      -qq
     apt-get autoclean       -qq
     rm -f /etc/apt/apt.conf.d/51cache
 EOF
 
   log "Creating a backup copy of the updated image ..."
+  log "Unmounting rootfs/boot ..."
   sudo umount rootfs/boot
+  log "Unmounting rootfs ..."
   sudo umount rootfs
+  log "Deleting partition mappings for"  "${temp_img}"
   sudo kpartx -dv $temp_img
   temp_img_backup=${temp_img//.img/-updated.img}
+  info "Setting backup copy filename to" "${temp_img_backup}"
   pv $temp_img > $temp_img_backup
-  sudo kpartx -av $temp_img
+
+  log "Done making backup copy."
+
+#  read -p "Return to continue, Q to quit ... "
+#  case $REPLY in
+#    Q|q)  cp $temp_img_password_file ${temp_img_password_file}-copy
+#          exit 0;;
+#    *) ;;
+#  esac
+
+  log "Creating partition mappings from image file ..."
+  read -a loop_maps <<< $( 
+    sudo kpartx -av $temp_img | \
+    awk '$0~/^add map/ { printf("%s ",$3) }'
+  )
+  debug 'loop_maps=( '"${loop_maps[@]}"' )'
 
   log "... Sleeping for 1 second ..."
   sleep 1
 
-  sudo mount /dev/mapper/loop0p2 rootfs
-  sudo mount /dev/mapper/loop0p1 rootfs/boot
+  sudo mount /dev/mapper/"${loop_maps[1]}" rootfs
+  sudo mount /dev/mapper/"${loop_maps[0]}" rootfs/boot
 
 fi
 
@@ -390,8 +358,9 @@ fi
 print_header "Customize Updated Image"
 
 log "Install additional packages ..."
-sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash -x <<-EOF
-  echo 'Acquire::http { Proxy "http://172.17.0.1:3142"; };' | tee /etc/apt/apt.conf.d/51cache
+sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash -x <<-EOF
+  echo 'Acquire::http { Proxy "http://172.17.0.1:3142"; };' | \
+    tee /etc/apt/apt.conf.d/51cache
   export http_proxy="http://172.17.0.1:3142"
   apt-get update          -qq
   apt-get install         -qq  ${additional_packages[@]}
@@ -399,7 +368,7 @@ sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash -x
 EOF
 
 log "Disable undesired systemd units/services in image ..."
-sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash -x <<-EOF
+sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash -x <<-EOF
   rm -vf /etc/systemd/system/multi-user.target.wants/3dprinteros*
   rm -vf /etc/systemd/system/multi-user.target.wants/avahi-daemon.service
   rm -vf /etc/systemd/system/sockets.target.wants/avahi-daemon.socket
@@ -408,21 +377,23 @@ sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash -x
 EOF
 
 log "Rsync'ing in local systemd unit to set unique hostname on boot ..."
-sudo rsync -avz /vagrant/src/set-unique-hostname-before-network/ rootfs/
+sudo rsync -rlptv /vagrant/src/set-unique-hostname-before-network/ rootfs/
+#sudo find rootfs/ -user 1000 -exec chown root:root {} + 2>/dev/null
 
 log "Enabling set-unique-hostname-before-network.service ..."
-sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash -x <<-EOF
+sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash -x <<-EOF
   mkdir -p  /etc/systemd/system/network.target.wants     && \
   cd        /etc/systemd/system/network.target.wants     && \
   ln -svf ../set-unique-hostname-before-network.service
 EOF
 
-set +e
-set +E
-log "Dropping you into the image's shell for any custom work ..."
-sudo systemd-nspawn -q --bind /usr/bin/qemu-arm-static -D ./rootfs/ /bin/bash
-set -e
-set -E
+log "Running saltstack-prep-and-install.sh ..."
+sudo ${SYSTEMD_NSPAWN_CMD} /vagrant/src/saltstack-prep-and-install.sh
+
+#log "Dropping you into the image's shell for any custom work ..."
+#warn "You can avoid errors on exit by using 'exit 0' not ^D"
+#sudo ${SYSTEMD_NSPAWN_CMD} /bin/bash
+
 
 # =====================================================================
 #
@@ -432,16 +403,14 @@ set -E
 
 print_header "Perform chattrs for Hack-y File Protections"
 
-files_to_chattr=(
-  "etc/passwd"
-  "etc/shadow"
-  "etc/ssh/sshd_config"
-  "root/.ssh/authorized_keys"
-  )
-
-for f in "${files_to_chattr[@]}"; do
-  [[ -e rootfs/${f} ]] && sudo chattr -V +i rootfs/${f}*
-done
+(
+  cd rootfs && \
+  if [[ "${#files_to_chattr[@]}" -gt 0 ]]; then
+    for file in "${files_to_chattr[@]}"; do
+      sudo chattr -V +i "${file}"
+    done
+  fi
+)
 
 
 # =====================================================================
---
+++ src/saltstack-prep-and-install.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+
+set -e
+
+
+# =====================================================================
+#
+# Global Variables
+#
+# =====================================================================
+
+SALTSTACK_VERSION="${SALTSTACK_VERSION:-2018.3.1}"
+
+LANG=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8
+
+export LANG LC_CTYPE
+
+
+# =====================================================================
+#
+# Functions
+#
+# =====================================================================
+
+function err_handler () {
+  # The behavior here has been changed from error() to warn()
+  # since in the context of this particular script the return
+  # of a non-zero value may not be an "error" per se and
+  # thus let us not mislead the reader ...
+  warn "The last command returned $?."
+}
+
+function cleanup_and_exit () {
+  set +x
+  print_header "Exiting & Cleanup"
+  log "Cleaning up before exit ..."
+
+  warn "( ... your commands here ... )"
+
+  echo -e '\n'${START}${BOLDHIGREEN}'Bye!'${END}
+}
+
+
+# =====================================================================
+#
+# Source additional "libs"
+#
+# =====================================================================
+
+source $(dirname $0)/lib-colors-logging.sh
+
+
+# =====================================================================
+#
+# Traps
+#
+# =====================================================================
+
+trap "err_handler"  ERR
+
+trap "cleanup_and_exit" EXIT
+
+
+# =====================================================================
+#
+# Remove user 'pi' if present
+#
+# =====================================================================
+
+print_header "Removing user 'pi'"
+
+if (id pi >/dev/null 2>/dev/null); then
+
+  read -a pi_groups <<< $(id -Gn pi)
+
+  log "Removing user 'pi' as member from from ${#pi_groups[@]} groups ..."
+  for gid in $"{pi_groups[@]}"; do 
+    info "Removing from group: ${gid} ..."
+    deluser pi $gid >/dev/null || warn "Error removing from ${gid}"
+  done
+
+  log "Deleting user 'pi' ..."
+  set +e; userdel -r pi; set -e
+
+  log "Deleting group 'pi' (as needed) ..."
+  set +e; delgroup pi; set -e
+
+  log "Deleting sudoers.d/010_pi-nopasswd ..."
+  rm -fv /etc/sudoers.d/010_pi-nopasswd
+
+else
+  warn "User 'pi' does not exist apparently. Skipping ..."
+fi
+
+
+# =====================================================================
+#
+# Make some security-related updates to /etc/ssh/sshd_config
+#
+# =====================================================================
+
+print_header "sshd Security Changes"
+
+if [[ ! -f /etc/ssh/sshd_config.orig ]]; then
+  log "Backing up /etc/ssh/sshd_config to /etc/ssh/sshd_config.orig"
+  cp -v /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+fi
+
+log "Setting PermitRootLogin to without-password ..."
+sed -i -E -e 's/^(#)?PermitRootLogin.*$/PermitRootLogin without-password/' /etc/ssh/sshd_config
+
+sshd_config_keywords_set_to_no=(
+  PasswordAuthentication
+  ChallengeResponseAuthentication
+  UsePAM
+  )
+for k in "${sshd_config_keywords_set_to_no}"; do
+  log "Setting ${k} to 'no' ..."
+  sed -i -E -e 's/^(#)?'$k'.*$/'$k' no/' /etc/ssh/sshd_config
+done
+
+log "diff of changes to sshd_config"
+set +e
+>&2 diff -uw /etc/ssh/sshd_config.orig /etc/ssh/sshd_config
+set -e
+
+
+# =====================================================================
+#
+# Add some ssh pubkeys to root user authorized_keys
+#
+# =====================================================================
+
+print_header "Adding root ssh pubkeys"
+(umask 0077 && mkdir -p /root/.ssh)
+for ssh_pubkey in /vagrant/ssh-pubkeys/*.pub; do
+  info "+ ${ssh_pubkey}"
+  cat $ssh_pubkey >> /root/.ssh/authorized_keys
+done
+
+
+# =====================================================================
+#
+# Enable sshd
+#
+# =====================================================================
+
+print_header "Enabling sshd"
+if [[ -f /lib/systemd/system/sshswitch.service ]]; then
+	info "Using the Raspbian way"
+  set -x; touch /boot/ssh.txt; set +x
+else
+	warn "Likely not a Raspbian-based image, using alt method ... "
+	set -x; update-rc.d ssh enable; set +x
+fi
+
+
+# =====================================================================
+#
+# Manual systemd service changes
+#
+# =====================================================================
+
+print_header "Additional systemd changes"
+
+log "Removing 'triggerhappy.service' symlink ..."
+set -x
+rm -fv /etc/systemd/system/multi-user.target.wants/triggerhappy.service
+set +x
+
+log "Done with additional systemd changes."
+
+
+# =====================================================================
+#
+# Additional packages
+#
+# =====================================================================
+
+print_header "Installing packages supporting 3DPrinterOS Client"
+
+addl_pkgs=(
+  virt-what
+  opencv-data
+  python-opencv
+  python-numpy
+  python-libusb1
+  libusb-1.0-0
+  python-notify2
+  fail2ban
+)
+
+log "Updating apt caches ..."
+apt-get update -qq
+
+log "Installing ${#addl_pkgs[@]} additional packages ..."
+for pkg in "${addl_pkgs[@]}"; do
+  info "Installing $pkg ..."
+  apt-get install --no-install-recommends -qq $pkg
+done
+
+log "Installation of additional packages completed."
+
+
+# =====================================================================
+#
+# Install Saltstack
+#
+# =====================================================================
+
+print_header "Install salt-minion"
+
+# use `lsb_release` values
+LSB_RELEASE="$(echo $(lsb_release -sr) 1 / p | dc)"
+debug "LSB_RELEASE: ${LSB_RELEASE}"
+
+LSB_CODENAME="$(lsb_release -sc)"
+debug "LSB_CODENAME: ${LSB_CODENAME}"
+
+DPKG_ARCH="$(dpkg --print-architecture)"
+debug "DPKG_ARCH: ${DPKG_ARCH}"
+
+log "Adding saltstack apt signing key ..."
+wget -O - https://repo.saltstack.com/apt/debian/${LSB_RELEASE}/${DPKG_ARCH}/archive/${SALTSTACK_VERSION}/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+
+log "Adding saltstack apt repo ..."
+cat <<EOF >/etc/apt/sources.list.d/saltstack.list
+deb http://repo.saltstack.com/apt/debian/${LSB_RELEASE}/${DPKG_ARCH}/archive/${SALTSTACK_VERSION} ${LSB_CODENAME} main
+EOF
+
+log "Updating apt cache ..."
+apt-get update
+
+log "Install salt-minion v${SALTSTACK_VERSION} ..."
+apt-get install --no-install-recommends -y salt-minion=${SALTSTACK_VERSION}+ds-1
+
+if (pgrep -c -f salt-minion >/dev/null); then
+  log "Killing running salt-minion ..."
+  pkill -f salt-minion
+fi
+
+log "(Force) remove minion_id file ..."
+rm -fv /etc/salt/minion_id
+
+
+# =====================================================================
+#
+# Done
+#
+# =====================================================================
+
+print_header "Done"
+
+exit 0
