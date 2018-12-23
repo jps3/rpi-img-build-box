5dd3269ffdfca2b215956543c79f7abc6e97d46f
Jason Stanford <jps3@lehigh.edu>
2018-12-23 15:58:32 -0500

    Cleaning up script and systemd units setting distinct hostname
     - Changed naming pattern from set-unique-hostname-before-network to
       set-distinct-hostname
    
     - Cleaned up shell script which performs the hostname change
    
     - Updated systemd unit files
    
     - Started framework for creating deb pkg

--- src/set-distinct-hostname/usr/local/sbin/set-distinct-hostname.sh
+++ src/set-distinct-hostname/usr/local/sbin/set-distinct-hostname.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
-set -e
-set -E
+DEFAULT_INTERFACE="$(route | awk '$1=="default" { print $NF }')"
+DEFAULT_INTERFACE_MAC="$(cat /sys/class/net/${DEFAULT_INTERFACE}/address)"
+MAC_STUB="$(echo $DEFAULT_INTERFACE_MAC | cut -d: -f4- | tr -d : | tr 'a-z' 'A-Z')"
+NEW_HOSTNAME="${HOSTNAME_PREFIX:-pi}-${MAC_STUB}"
 
-NETWORK_IFACE_MAC_NIC="$(cat /sys/class/net/${NETWORK_IFACE}/address | cut -d: -f4- | tr -d : | tr 'a-z' 'A-Z')"
-NEW_HOSTNAME="${HOSTNAME_PREFIX}-${NETWORK_IFACE_MAC_NIC}"
 
 #
 # Simple check that results in existing with an error if any spaces
@@ -14,20 +14,18 @@ NEW_HOSTNAME="${HOSTNAME_PREFIX}-${NETWORK_IFACE_MAC_NIC}"
 #
 echo "$NEW_HOSTNAME" | grep -v -q '[[:space:]]'
 
+
 #
 # Set the new hostname
 #
-echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
-echo $NEW_HOSTNAME > /etc/hostname
-/bin/hostname -F /etc/hostname
+hostnamectl set-hostname $NEW_HOSTNAME
+
 
 #
 # Update /etc/hosts 127.0.1.1
 #
 sed -i -e '/^127\.0\.1\.1/ s/raspberrypi/'${NEW_HOSTNAME}'/' /etc/hosts
 
-set +E
-set +e
 
 exit 0
 
