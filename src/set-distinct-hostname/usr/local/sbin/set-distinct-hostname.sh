e13994115a1040749663b5ab9ad06c72a519c6c5
Jason Stanford <jps3@lehigh.edu>
2018-12-25 14:08:39 -0500

    Fixed set-distinct-hostname systemd unit and script

--- src/set-distinct-hostname/usr/local/sbin/set-distinct-hostname.sh
+++ src/set-distinct-hostname/usr/local/sbin/set-distinct-hostname.sh
@@ -3,7 +3,20 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 DEFAULT_INTERFACE="$(route | awk '$1=="default" { print $NF }')"
-DEFAULT_INTERFACE_MAC="$(cat /sys/class/net/${DEFAULT_INTERFACE}/address)"
+
+SYS_PATH="/sys/class/net/${DEFAULT_INTERFACE}/address"
+if [[ -e "${SYS_PATH}" ]]; then
+	DEFAULT_INTERFACE_MAC="$(cat $SYS_PATH)"
+else
+	echo "$0: $SYS_PATH does not exist." | systemd-cat
+	exit -1
+fi
+
+if [[ -z "$DEFAULT_INTERFACE_MAC" ]]; then
+	echo "$0: DEFAULT_INTERFACE_MAC is empty." | systemd-cat
+	exit -2
+fi
+
 MAC_STUB="$(echo $DEFAULT_INTERFACE_MAC | cut -d: -f4- | tr -d : | tr 'a-z' 'A-Z')"
 NEW_HOSTNAME="${HOSTNAME_PREFIX:-pi}-${MAC_STUB}"
 
