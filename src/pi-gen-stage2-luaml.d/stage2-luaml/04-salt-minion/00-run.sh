7a3fb63d3893934af653ef757f5cfb2a2e2fc601
Jason Stanford <jps3@lehigh.edu>
2019-03-17 14:44:58 -0400

    Added explicit systemctl enable and stop for salt-minion
     * MInor white space cleanup (tabs -> spaces)
     * Added explicit `systemctl enable salt-minion` rather than leaving, or trusting, that default will be enabled. (Regardless of whether `-X` flag passed to installer script is working as intended or not)
     * Added explicit `service ... stop` and `pkill` for salt-minion to prevent later errors building EXPORT_* items due to open file handles.

--- src/pi-gen-stage2-luaml.d/stage2-luaml/04-salt-minion/00-run.sh
+++ src/pi-gen-stage2-luaml.d/stage2-luaml/04-salt-minion/00-run.sh
@@ -12,10 +12,10 @@ echo "# ------------------------------------------------------------ #"
 on_chroot << EOF
 set -x
 if ! (dpkg-query --show salt-minion); then 
-	curl -sLf http://bootstrap.saltstack.com | \
-		/bin/sh -s -- -X -F -A $SALT_MASTER $SALTSTACK_VERSION
+  curl -sLf http://bootstrap.saltstack.com | \
+    /bin/sh -s -- -X -F -A $SALT_MASTER $SALTSTACK_VERSION
 else
-	echo "salt-minion already installed (skipping this step)"
+  echo "salt-minion already installed (skipping this step)"
 fi
 set +x
 EOF
@@ -29,13 +29,23 @@ set -x
 rm -fv /etc/salt/minion_id
 rm -fv /etc/salt/pki/minion/minion.p*
 if ! $SALT_ENABLED; then
-	systemctl disable salt-minion || \
-	  find /etc/systemd/system -name salt-minion.service -type l -delete
+  systemctl disable salt-minion || \
+    find /etc/systemd/system -name salt-minion.service -type l -delete
 else
-    echo "Leaving salt-minion service enabled (installer default) ..."
+  echo "Ensuring salt-minion service is enabled ..."
+  systemctl enable salt-minion
 fi
 set +x
 EOF
+
+set +e
+on_chroot << EOF
+set -x
+service salt-minion stop
+pkill -U root -f /usr/bin/salt-minion
+set +x
+EOF
+set -e
 
 echo "# ------------------------------------------------------------ #"
 echo "# end of saltstack installation script"
