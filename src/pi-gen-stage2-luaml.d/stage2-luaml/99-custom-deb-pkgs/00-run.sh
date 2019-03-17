4b5fa37373f9fd94f6b7df65dee63af21d85a708
Jason Stanford <jps3@lehigh.edu>
2019-03-17 14:21:38 -0400

    Fixed and cleaned up stage2-luaml run script
     * Minor whitespace reformatting (tabs -> spaces)
     * Added informational/contextual output blocks
     * Changed from manually adding symlink for `set-distinct-hostname.service`in `network.target.wants` to using `systemctl enable ...`
     * Fixed issue preventing `HOSTNAME_PREFIX` from being set in `/etc/default/set-distinct-hostname-service` file.

--- src/pi-gen-stage2-luaml.d/stage2-luaml/99-custom-deb-pkgs/00-run.sh
+++ src/pi-gen-stage2-luaml.d/stage2-luaml/99-custom-deb-pkgs/00-run.sh
@@ -1,24 +1,48 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
 cp -vf files/*.deb "${ROOTFS_DIR}/tmp/"
 
+echo "# ------------------------------------------------------------ #"
+echo "# INFO"
+echo "#"
+echo "# $0"
+echo "#"
+echo "# ------------------------------------------------------------ #"
+
 on_chroot << EOF
+set -x
 cd /tmp/
 pwd
 ls -1 *.deb
 for deb in *.deb; do
-	dpkg -i \$deb
+  dpkg -i \$deb
 done
+set +x
 EOF
 
-on_chroot << EOF
-set -e
-cd /etc/systemd/system/
-[[ ! -d network.target.wants ]] && mkdir -m 755 network.target.wants
-cd network.target.wants
-ln -svf ../set-distinct-hostname.service
-EOF
 
 on_chroot << EOF
-sed -i -e 's/^HOSTNAME_PREFIX=.*/HOSTNAME_PREFIX="${HOSTNAME_PREFIX}"/' /etc/default/set-distinct-hostname-service
+set -x
+systemctl enable set-distinct-hostname.service
+set +x
 EOF
+
+
+echo "# ------------------------------------------------------------ #"
+echo "# INFO"
+echo "#"
+echo "# HOSTNAME_PREFIX    : '${HOSTNAME_PREFIX}'"
+echo "#"
+echo "# ------------------------------------------------------------ #"
+
+if [[ -n "${HOSTNAME_PREFIX}" ]]; then
+  set -x
+  sed -i \
+    -e 's/^HOSTNAME_PREFIX=.*/HOSTNAME_PREFIX="'${HOSTNAME_PREFIX}'"/' \
+    "${ROOTFS_DIR}/etc/default/set-distinct-hostname-service"
+  set +x
+fi
+
+echo "# ------------------------------------------------------------ #"
+echo "#  END"
+echo "# ------------------------------------------------------------ #"
