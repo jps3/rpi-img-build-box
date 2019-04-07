f9b347a2c4ffef11cc27918d708b8b173644708f
Jason Stanford <jps3@lehigh.edu>
2019-04-07 19:54:30 -0400

    Removed hardcoded ssh authorized_keys file to using config-* file

--- src/pi-gen-stage2-luaml.d/stage2-luaml/02-sshd-config/00-run.sh
+++ src/pi-gen-stage2-luaml.d/stage2-luaml/02-sshd-config/00-run.sh
@@ -24,9 +24,30 @@ touch "${ROOTFS_DIR}/boot/ssh.txt"
 install -v -m 755 -d                    "${ROOTFS_DIR}/root/.ssh"
 install -v -m 600 files/authorized_keys "${ROOTFS_DIR}/root/.ssh/"
 
-for p in "${SSH_PUBKEYS[@]}"; do
-	printf '%s\n' "$p" | tee -a "${ROOTFS_DIR}/root/.ssh/authorized_keys"
-done
+log "    # ------------------------------------------------------------ #"
+log "    # SSH_PUBKEYS[] : ${#SSH_PUBKEYS[@]} keys defined"
+if [[ "${#SSH_PUBKEYS[@]}" -gt 0 ]]; then
+	i=0
+	for k in "${SSH_PUBKEYS[@]}"; do
+		i=$(( $i + 1 ))
+		log "        $(printf "%02i %-s" $i "${p}")"
+		echo "$p" >> "${ROOTFS_DIR}/root/.ssh/authorized_keys"
+	done
+fi
+log "    # "
+log "    # ------------------------------------------------------------ #"
 
+if [[ "${#SSH_PUBKEYS[@]}" -eq 0 ]]; then
+	log "    "
+	log "    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #"
+	log "    # "
+	log "    #       W A R N I N G"
+	log "    #       YOU DO NOT HAVE ANY INITIAL SSH PUBKEYS DEFINED"
+	log "    # "
+	log "    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #"
+	log "    "
+	log "    ... sleeping for 120 sec ... ^C to kill/quit build ..."
+	sleep 120
+fi
 
 
